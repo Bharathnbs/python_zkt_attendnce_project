2d1a0081b8c32f9ef412c447dec57849e1b4021d
Bharath N <109462055+Bharathnbs@users.noreply.github.com>
2024-04-30 16:50:06 +0530

    attendance  data - data send request api

--- index.py
+++ index.py
@@ -1,15 +1,29 @@
-
 from zk import *
+import requests
+import json
+
 machine_port = 4370
 z = ZK('192.168.31.51', port=machine_port, force_udp=False, ommit_ping=False)
 conn = z.connect()
 z.enable_device()
 z.live_capture()
-attendances = conn.get_attendance()
-conn.clear_attendance()
+
+api_url = "https://webhook.site/65e348fd-94f8-4364-9801-0045d2f4c825"
+
 for attendance in conn.live_capture():
     if attendance is None:
         pass
     else:
-        print (attendance)
+        attendance_data = {
+            'data': str(attendance) 
+        }
 
+        json_data = json.dumps(attendance_data)
+
+        response = requests.post(api_url, json=json_data)
+        
+        if response.status_code == 200:
+            print("Attendance entry sent successfully.")
+        else:
+            print("Failed to send attendance entry. Status code:", response.status_code)
+        print(attendance)
