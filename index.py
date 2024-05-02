f5d94024f414532d3fe12145b8e65cb1bd4350d5
Bharath N <109462055+Bharathnbs@users.noreply.github.com>
2024-05-02 18:08:56 +0530

    attendance log format get and send api

--- index.py
+++ index.py
@@ -8,16 +8,41 @@ conn = z.connect()
 z.enable_device()
 z.live_capture()
 
-api_url = "https://webhook.site/65e348fd-94f8-4364-9801-0045d2f4c825"
-
+attendance_data = {
+    "emp_id": "",
+    "status": "",
+    "punch": ""
+}   
+   
+attendances = conn.get_attendance()
+conn.clear_attendance()
 for attendance in conn.live_capture():
-    if attendance is None:
-        pass
-    else:
-        attendance_data = {
-            'data': str(attendance) 
-        }
-
+    if attendance is not None:
+        match attendance.status:
+            case 0:
+                attendance_data["emp_id"] = attendance.user_id
+                attendance_data["status"] = "password"
+                if attendance.punch == 0:
+                    attendance_data["punch"] = "checkin"
+                else:
+                    attendance_data["punch"] = "checkout"
+            case 1:
+                attendance_data["emp_id"] = attendance.user_id
+                attendance_data["status"] = "finger"
+                if attendance.punch == 0:
+                    attendance_data["punch"] = "checkin"
+                else:
+                    attendance_data["punch"] = "checkout"
+            case 2:
+                attendance_data["emp_id"] = attendance.user_id
+                attendance_data["status"] = "rfid"
+                if attendance.punch == 0:
+                    attendance_data["punch"] = "checkin"
+                else:
+                    attendance_data["punch"] = "checkout"
+        
+        api_url = "https://webhook.site/65e348fd-94f8-4364-9801-0045d2f4c825"
+        
         json_data = json.dumps(attendance_data)
 
         response = requests.post(api_url, json=json_data)
@@ -26,4 +51,5 @@ for attendance in conn.live_capture():
             print("Attendance entry sent successfully.")
         else:
             print("Failed to send attendance entry. Status code:", response.status_code)
-        print(attendance)
+
+        print(attendance_data)
